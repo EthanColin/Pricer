43e13623a3f8fe43c795087548bcba02f9dd3f21
EthanColin <72448951+EthanColin@users.noreply.github.com>
2021-07-06 11:32:29 +0800

    Update price_cal.py

--- price_cal.py
+++ price_cal.py
@@ -1,1 +1,14 @@
+from pricer_helpers import *
 
+
+def main():
+    cos = cal_cos(unit_price, unit_transport_fee, unit_shipping_fee)
+    sp = cal_sp(cos, unit_profit_rate)
+    profit = cal_profit(sp, cos)
+
+    print("\nCost of Sales: " + str(round(cos, 2)) + " (RM" + str(round(cal_exc(cos), 2)) + ")")
+    print("Selling Price: " + str(round(sp, 2)) + " (RM" + str(round(cal_exc(sp), 2)) + ")")
+    print("Profit: " + str(round(profit, 2)) + " (RM" + str(round(cal_exc(profit), 2)) + ")")
+
+
+main()
